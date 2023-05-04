182c6f669d476e89f773aec0b3b1819b11d807b6
riquejoha <riquelme.johanna07@gmail.com>
2023-05-04 11:13:25 -0300

    primer paso

---
+++ script.js
@@ -0,0 +1,26 @@
+const pagina = document.querySelector("body");
+const selectOpcionesColor = document.querySelector("select");
+
+function cambiarColorTexto(){
+    switch (parseInt(selectOpcionesColor.value)) {
+        case 1:
+            pagina.style.color = "white";
+            pagina.style.backgroundColor = "black";
+            break;
+        case 2:
+            pagina.style.color = "violet";
+            pagina.style.backgroundColor = "yellow";
+            break;
+        case 3:
+            pagina.style.color = "pink";
+            pagina.style.backgroundColor = "red";
+            break;        
+
+        default:
+            alert = "ERROR";
+    }
+
+
+
+
+}
